800ddad145cd08ad4d532c73b2cdbaaf54f69e46
Gijs van der Munnik <gijs@munnik.net>
2022-05-20 16:32:12 +0200

    first commit

---
+++ Index.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * Name : Gijs van der Munnik
+ * Date : 05/04/2022
+ *
+ * Homepage Project 3
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>KW1C</title>
+        <link rel="icon" href="Images/favicon.ico">
+        <link rel="stylesheet" href="Styles\stylesheet.css">
+    </head>
+    <body>
+    <?php
+    include "Includes/header.php";
+    ?>
+        <main>
+            <div class="lijn">
+
+            </div>
+            <h1 id="acedemy">
+                ICT-Academie
+            </h1>
+            <article>
+                <h2 id="theh2">
+                                Algemeen
+                </h2>
+                <p id="text">
+                                Een goede ICT’er is gedreven om te leren en is bereid om kennis te delen met anderen. Dat vinden ze bij het bedrijfsleven. Verder moet je wat willen bereiken, plezier hebben in wat je doet en goed in een team kunnen werken. Tijdens je studie aan de ICT-Academie krijg je hiermee te maken. Dat gebeurt zoveel mogelijk in projectvormen: leren werken en werkend leren in klein groepsverband, met opdrachten uit de praktijk. Teamwork dus. Belangrijk is verder de adviesraad, die bestaat uit vertegenwoordigers van vooraanstaande bedrijven uit de regio.
+                </p>
+            </article>
+            <?php
+                include "Includes/footer.php"
+            ?>
+        </main>
+    </body>
+</html>
